6b22ce95cf089aacdc320234b5b7c26f707299de
Stivenprod <asdm1098@gmail.com>
2022-04-04 18:04:43 -0500

    test env dev

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -2,27 +2,27 @@ import firebase from "firebase/app";
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBxVLEC6yC52syEeqpDL3_l5sayGf7-JJs",
-//   authDomain: "post-app-8eef6.firebaseapp.com",
-//   projectId: "post-app-8eef6",
-//   storageBucket: "post-app-8eef6.appspot.com",
-//   messagingSenderId: "848184802503",
-//   appId: "1:848184802503:web:659f7c6f2615b5afe7d2e2"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBxVLEC6yC52syEeqpDL3_l5sayGf7-JJs",
+  authDomain: "post-app-8eef6.firebaseapp.com",
+  projectId: "post-app-8eef6",
+  storageBucket: "post-app-8eef6.appspot.com",
+  messagingSenderId: "848184802503",
+  appId: "1:848184802503:web:659f7c6f2615b5afe7d2e2"
+};
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 
 
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID
-  };
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_APIKEY,
+//     authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//     projectId: process.env.REACT_APP_PROJECTID,
+//     storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//     appId: process.env.REACT_APP_APPID
+//   };
 
   //variable de entorno verlas
  // console.log(process.env);
